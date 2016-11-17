2c3250e33eefe33b249dd9769624a5d8d381ab65
Akshay Sonvane <akshay.sonvane93@gmail.com>
2016-11-16 18:53:29 -0800

    Updated connection mode

--- CS unplugged activity samples/greenfoot-sample-project/GameMode.java
+++ CS unplugged activity samples/greenfoot-sample-project/GameMode.java
@@ -1,28 +1,20 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import javax.swing.JOptionPane;
 
-/**
- * Write a description of class EnterName here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class EnterName extends Actor
+public class GameMode extends Actor
 {
     public void act() 
     {
         if(Greenfoot.mouseClicked(this))
         {
-            // String userId = JOptionPane.showInputDialog("Enter name");
-            // DataModel.getInstance().setUserId(userId);
-            
             if(Utils.getInstance().getGameHost())
             {
                 Utils.getInstance().setGameHost(false);
+                this.setImage("button_connect-mode.png");
             }
             else
             {
                 Utils.getInstance().setGameHost(true);
+                this.setImage("button_host-mode.png");
             }
         }
     }    
