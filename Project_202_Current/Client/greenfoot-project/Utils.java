d34ad81b74e46fc7869ae319cb6b7ad476284a40
Akshay Sonvane <akshay.sonvane93@gmail.com>
2016-12-03 17:44:01 -0800

    Change utils

--- Project_202_Current/Client/greenfoot-project/Utils.java
+++ Project_202_Current/Client/greenfoot-project/Utils.java
@@ -1,7 +1,7 @@
 public class Utils  
 {
     private static Utils utils = null;
-    public static String BASE_URL = "http://localhost:8080/MultiPlayerGame/";
+    public static String BASE_URL = "http://ec2-54-183-114-75.us-west-1.compute.amazonaws.com:8080/MultiPlayerGame/";
     public static int BOUND = 12;
     private boolean gameHost = false;
 
